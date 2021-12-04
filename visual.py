0cbf6b9cb88ade792427abd69833d3ebc7fd29e1
Pratyaksha Gupta <82153437+Pratyaksha-047@users.noreply.github.com>
2021-12-04 12:04:08 +0530

    Add files via upload

---
+++ visual.py
@@ -0,0 +1,67 @@
+EMPTY_CELL_COLOR ="#fcd7d6"
+
+SCORE_LABEL_FONT = ("Verdana",24)
+SCORE_FONT = ("Helvetica",36, "bold")
+GAME_OVER_FONT = ("Helvetica",48, "bold")
+GAME_OVER_COLOR = "#ffffff"
+GRID_COLOR = "#084a23"
+
+WINNER_BG = "#ffcc09"
+LOSER_BG = "#a39489"
+
+
+LABEL_FONT = ("Verdana", 40, "bold")
+
+GAME_COLOR = "#a6bdbb"
+
+EMPTY_COLOR = "#8eaba8"
+
+SCORE_COLOR="#000000"
+
+CELL_COLORS = {
+    2: "#daeddf", 
+    4: "#9ae3ae", 
+    8: "#6ce68d", 
+    16: "#42ed71",
+    32: "#17e650", 
+    64: "#17c246", 
+    128: "#149938",
+    256: "#107d2e", 
+    512: "#0e6325",
+    1024: "#0b4a1c",
+    2048: "#031f0a", 
+    4096: "#000000", 
+    8192: "#000000",
+    }
+
+CELL_NUMBER_COLORS = {
+    2: "#011c08", 
+    4: "#011c08", 
+    8: "#011c08", 
+    16: "#011c08",
+    32: "#011c08", 
+    64: "#f2f2f0", 
+    128: "#f2f2f0",
+    256: "#f2f2f0",
+    512: "#f2f2f0", 
+    1024: "#f2f2f0",
+    2048: "#f2f2f0",
+    4096: "#f2f2f0", 
+    8192: "#f2f2f0",
+    }
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica",55, "bold"), 
+    4: ("Helvetica",55, "bold"), 
+    8: ("Helvetica",55, "bold"), 
+    16: ("Helvetica",50, "bold"),
+    32: ("Helvetica",50, "bold"), 
+    64: ("Helvetica",50, "bold"), 
+    128: ("Helvetica",45, "bold"),
+    256: ("Helvetica",45, "bold"),
+    512: ("Helvetica",45, "bold"), 
+    1024: ("Helvetica",40, "bold"),
+    2048: ("Helvetica",40, "bold"),
+    4096: ("Helvetica",40, "bold"), 
+    8192: ("Helvetica",40, "bold"),
+    }
